d78b507e1ca49e2cd64264f045ec7db224e40d20
AzureAD\SarahRedway <sarah.redway@send.technology>
2021-06-10 16:32:39 +0100

    hot key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   licenseKeys: {
-    handsontable: '65f77-b0309-c66f0-24c16-af808',
+    handsontable: 'non-commercial-and-evaluation',
   },
 };
